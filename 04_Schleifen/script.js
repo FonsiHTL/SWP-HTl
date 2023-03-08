198ade95188f9c59a211abf8a5c375a3ab6f619d
Leander <leander.wachter@student.htldornbirn.at>
2023-03-08 09:33:17 +0100

    Startet Loop Exercise

---
+++ 04_Schleifen/script.js
@@ -0,0 +1,15 @@
+for (let count1 = 7; count1 <= 45; count1++) {
+    console.log("Ausgabe");
+}
+
+for (let count2 = 9; count2 <= 99; count2++) {
+    console.log("Ausgabe");
+}
+
+for (let count3 = 10; count3 >= 0; count3--) {
+    console.log("Ausgabe");
+}
+
+for (let count1 = 7; count1 < 45; count1++) {
+    console.log("Ausgabe");
+}
