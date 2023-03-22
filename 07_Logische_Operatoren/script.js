ea505bd9383468e232c12c25a045ab1b135e114a
Leander <leander.wachter@student.htldornbirn.at>
2023-03-22 09:29:23 +0100

    07_Logische_Operatoren finished

---
+++ 07_Logische_Operatoren/script.js
@@ -0,0 +1,13 @@
+let randomNumber = Math.random() * 100;
+let random = Math.floor(randomNumber);
+
+console.log(random);
+
+let randomNumber2 = Math.random() * 100;
+let random2 = Math.floor(randomNumber2);
+
+
+
+if(random<50 && random<random2) {
+    console.log ("Zahl 1 ist kleiner als Zahl 2 und Mini");
+}
