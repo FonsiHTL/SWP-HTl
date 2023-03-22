39f3194d5102597ff17a9542e2c1ce9e5b48bb16
Leander <leander.wachter@student.htldornbirn.at>
2023-03-22 08:27:52 +0100

    04_Schleifen finished

--- 04_Schleifen/script.js
+++ 04_Schleifen/script.js
@@ -10,6 +10,10 @@ for (let count3 = 10; count3 >= 0; count3--) {
     console.log("Ausgabe");
 }
 
-for (let count1 = 7; count1 < 45; count1++) {
+for (let count4 = 1234; count4 >= 1207; count4--) {
     console.log("Ausgabe");
+}
+
+for (let count5 = 1; count5 <= 8; count5++) {
+    console.log(count5 + " Leander");
 }
