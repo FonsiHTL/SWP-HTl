74eea3c52507c268f3574f892711f541ed7ac1e6
Leander <leander.wachter@student.htldornbirn.at>
2023-03-22 08:44:13 +0100

    05_If_Bedingungen finished

---
+++ 05_If_Bedingung/script.js
@@ -0,0 +1,26 @@
+let number = 80;
+
+if(number<20){
+    console.log("Mini");
+}
+
+if(20<number && number<50){
+    console.log("Medium");
+}
+
+if(number>50){
+    console.log("Large");
+}
+
+
+if(number<=7){
+    console.log("Mini");
+}
+
+if(7<number && number<90){
+    console.log("Medium");
+}
+
+if(number>90){
+    console.log("Large");
+}
