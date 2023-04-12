f4aa31f1e9a9a9433fe035ea0e57886b50b05a08
Leander <leander.wachter@student.htldornbirn.at>
2023-04-12 08:58:42 +0200

    07_Logische_Operatoren finished

--- 07_Logische_Operatoren/script.js
+++ 07_Logische_Operatoren/script.js
@@ -11,3 +11,12 @@ let random2 = Math.floor(randomNumber2);
 if(random<50 && random<random2) {
     console.log ("Zahl 1 ist kleiner als Zahl 2 und Mini");
 }
+
+/*if(random<50 || random2<50) {
+console.log ("Eine der beiden ist kleiner als 30");
+}
+*/
+
+if(random<50 && random2!=50) {
+console.log ("Erste Zahl klein, zweite kein 50iger");
+}
