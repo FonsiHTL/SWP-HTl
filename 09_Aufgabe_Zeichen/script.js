e68f17635702314b83a91c7122aa79ba732b923f
Leander <leander.wachter@student.htldornbirn.at>
2023-04-19 14:19:01 +0200

    09_Aufgabe_Zeichen finished after a little mistake

---
+++ 09_Aufgabe_Zeichen/script.js
@@ -0,0 +1,33 @@
+let a = 0;
+let b = 0;
+
+for (let index = 1; index < 3; index ++) {
+    console.log("XXXX");
+    console.log("OOOO");
+    console.log("XXXX");
+    console.log("OOOO");
+    if (index%1==0) {
+        console.log("")
+    }
+}
+
+for (let index2 = 1; index2 < 101; index2 ++) {
+    if(index2%1==0){
+        a = a + index2;
+    }
+    if(index2%2==0){
+        b = b + index2;
+    }
+}
+
+console.log(a);
+console.log(b);
+
+for (let index3 = 1; index3 < 2; index3 ++) {
+    console.log("X      ");
+    console.log("X      ");
+    console.log("X      ");
+    console.log("X      ");
+    console.log("X      ");
+    console.log("X X X X")
+}
