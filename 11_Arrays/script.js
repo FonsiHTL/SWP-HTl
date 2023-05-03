89b15d4870dab04650c3ea7e0e3ecbddb0d04554
Leander <leander.wachter@student.htldornbirn.at>
2023-05-03 09:11:30 +0200

    12_Input_from_Shell Copied Demo-Code

--- 11_Arrays/script.js
+++ 11_Arrays/script.js
@@ -31,5 +31,5 @@ let freunde = ["Susi", "Paula", "Hans"];
 console.log("Meine Freunde sind " + freunde[0] + ", " + freunde[1] + " und " + freunde[2]+"!")
 console.log("")
 
-freunde.push("Sepp")
+freunde.push("Sepp");
 console.log("Meine Freunde sind " + freunde[0] + ", " + freunde[1] + ", " + freunde[2] + " und " + freunde[3]+"!")
