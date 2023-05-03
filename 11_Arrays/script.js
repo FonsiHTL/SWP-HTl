39ead614da4095cd9155ebb9f94f15ab347eaf33
Leander <leander.wachter@student.htldornbirn.at>
2023-05-03 08:52:26 +0200

    11_Arrays finished

---
+++ 11_Arrays/script.js
@@ -0,0 +1,35 @@
+
+/*
+let a = 1
+let b = 2
+let c = 3
+let d = 4
+
+let arr = [1,2,3,4];
+
+arr.push(17);
+arr.push(177);
+
+let summe = 0;
+
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index])
+    summe = summe + arr[index]
+}
+
+console.log("")
+console.log(summe)
+console.log("")
+console.log(summe / arr.length)
+console.log("")
+    
+*/
+
+
+let freunde = ["Susi", "Paula", "Hans"];
+
+console.log("Meine Freunde sind " + freunde[0] + ", " + freunde[1] + " und " + freunde[2]+"!")
+console.log("")
+
+freunde.push("Sepp")
+console.log("Meine Freunde sind " + freunde[0] + ", " + freunde[1] + ", " + freunde[2] + " und " + freunde[3]+"!")
