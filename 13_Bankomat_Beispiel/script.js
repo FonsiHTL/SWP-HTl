6dfd7950a13bfc211b37df6544592c79bcdeb6b1
Leander <leander.wachter@student.htldornbirn.at>
2023-05-17 09:11:18 +0200

    13_Bankomat finished

--- 13_Bankomat_Beispiel/script.js
+++ 13_Bankomat_Beispiel/script.js
@@ -14,17 +14,54 @@ const readLineAsync = () => {
   });
 };
 
-console.log("Enter your name");
+console.log("Bitte Ihren Namen eingeben:");
 let inputOfUser = await readLineAsync();
-console.log("Your name is " + inputOfUser);
+console.log("Dein Benutzername ist " + inputOfUser);
 
 
 let running = true;
 let balance = 0;
 
 console.log("Wählen Sie die gewünschte Aktion aus");
-console.log("1. Einzahlen");
-console.log("2. Auszahlen");
-console.log("3. Auszahlen und Einzahlen");
-console.log("4. Ende");
+console.log("1 = Einzahlen");
+console.log("2 = Auszahlen");
+console.log("3 = Kontostand");
+console.log("4 = Ende");
+
+while (running){
+  let action = await readLineAsync();
+ 
+
+  if (action === "1"){
+      console.log("Betrag, den Sie einzahlen möchten eingeben:")
+      let deposit = await readLineAsync();
+      balance += deposit;
+      console.log("Sie haben " + deposit + " Euro eingezahlt.")
+  }
+  
+  if (action === "2") {
+    console.log("Betrag, den Sie abheben möchten eingeben:");
+    let withdraw = await readLineAsync();
+    if (withdraw <= balance) {
+      balance -= withdraw;
+      console.log("Sie haben " + withdraw + " Euro abgehoben.");
+    } else {
+      balance = 0;
+      console.log("Nicht genug Geld auf dem Konto. Ihr Kontostand wurde auf 0 Euro gesetzt.");
+    }
+  }
+  
+  if (action === "3"){
+      console.log("Ihr Kontostand beträgt: " + balance + " Euro.");
+  }
+  
+
+  if (action === "4"){
+      console.log("Tschüss Tschau Ammannbau!")
+      running = false;
+  }
+  
+}
+
+readline.close();
 
