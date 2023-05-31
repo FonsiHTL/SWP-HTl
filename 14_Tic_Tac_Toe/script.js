d7710624f2274435807bedc1d9b06d9296cffd04
Leander <leander.wachter@student.htldornbirn.at>
2023-05-31 09:29:16 +0200

    14_Tic_Tac_Toe

--- 14_Tic_Tac_Toe/script.js
+++ 14_Tic_Tac_Toe/script.js
@@ -1,26 +1,48 @@
 import { createInterface } from "readline";
-let field = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-];
 
-
-
-const readline = createInterface({
+let readline = createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
-const readLineAsync = () => {
-  return new Promise((resolve) => {   
+let readLineAsync = () => {
+  return new Promise((resolve) => {
     readline.question("", (userRes) => {
       resolve(userRes);
-      //readline.close();
     });
   });
 };
 
+console.log("Enter your name");
+let inputOfUser = await readLineAsync();
+console.log("Your name is " + inputOfUser);
 
-  
+
+
+let field = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+];
+
+for (let row = 0; row < array.length; row++) {
+
+  let actualRow = field[row];
+  for (let col = 0; col < actualRow; col++) {
+      console.log(actualRow[col]);
+    
+
+        output =+ actualRow[col] + " ";
+
+  }
+        console.log(output);
+
+  console.log(field[actualRow]);
+}
+
+    field[0][1] = 1;
+
+
+
   function checkWinner(board)
+
