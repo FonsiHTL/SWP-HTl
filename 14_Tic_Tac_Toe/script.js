c3ed0a04312653359122657c39961ec2bb2a487a
andreas riedmann <andreas@riedmann.rocks>
2023-06-14 09:09:46 +0200

    fixes by ran

--- 14_Tic_Tac_Toe/script.js
+++ 14_Tic_Tac_Toe/script.js
@@ -2,7 +2,7 @@ import { createInterface } from "readline";
 
 let readline = createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 let readLineAsync = () => {
@@ -13,48 +13,59 @@ let readLineAsync = () => {
   });
 };
 
-console.log("Enter your name");
-let inputOfUser = await readLineAsync();
-console.log("Your name is " + inputOfUser);
-
-
 
 let field = [
   [0, 0, 0],
   [0, 0, 0],
-  [0, 0, 0]
+  [0, 0, 0],
 ];
 
-for (let row = 0; row < array.length; row++) {
 
-  let actualRow = field[row];
-  for (let col = 0; col < actualRow; col++) {
-      console.log(actualRow[col]);
-    
+// Hazuptprogramm
+console.log("Enter your name");
+let inputOfUser = await readLineAsync();
+console.log("Your name is " + inputOfUser);
 
-        output =+ actualRow[col] + " ";
+field[0][1] = 1;
+field[2][2] = 1;
+field[1][1] = 1;
 
+printField();
+
+
+function printField(){
+  for (let row = 0; row < field.length; row++) {
+    let actualRow = field[row];
+    let output="";
+    for (let col = 0; col < field.length; col++) {
+      
+  
+      output += field[row][col] + " | ";
+    }
+    console.log("x" + output);
+  
+   
   }
-        console.log(output);
-
-  console.log(field[actualRow]);
 }
 
-    field[0][1] = 1;
+function checkWinner(board) {
+  for (var row = 0; row < 3; row++) {
+    if (
+      board[row][0] != "" &&
+      board[row][0] == board[row][1] &&
+      board[row][1] == board[row][2]
+    ) {
+      return board[row][0];
+    }
+  }
 
-
-
-    function checkWinner(board) 
-      for (var row = 0; row < 3; row++) {
-        if (board[row][0] != "" && board[row][0] == board[row][1] && board[row][1] == board[row][2]) {
-          return board[row][0];
-        }
-      }
-
-      for (var col = 0; col < 3; col++) {
-        if (board[0][col] != "" && board[0][col] == board[1][col] && board[1][col] == board[2][col]) {
-          return board[0][col];
-        }
-      }
-
-      
+  for (var col = 0; col < 3; col++) {
+    if (
+      board[0][col] != "" &&
+      board[0][col] == board[1][col] &&
+      board[1][col] == board[2][col]
+    ) {
+      return board[0][col];
+    }
+  }
+}
